2ba2a0b19e5cc0bde1769c1c0b481ea56efc842d
Afsha Irshad <afshairshad2973@gmail.com>
2024-12-07 02:48:46 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from "./pages/Home/HomePage"
 import LoginPage from "./pages/LoginPage"
 import SignUpPage from "./pages/SignUpPage"
-import Footer from './components/footer';
+import Footer from './components/Footer';
 import { Toaster } from 'react-hot-toast';
 import { useAuthStore } from './store/authUser';
 import { useEffect } from 'react';
